18c2b98b7db3b47714db30f7cabdb8c8970d9ffe
Nahuel <Nahuelpardo3@gmail.com>
2025-05-11 19:19:15 -0300

    Agrego el validador de persona y cambios en el repositorio de persona

--- CentroEventos/CentroEventos.Aplicacion/Entidades/Persona.cs
+++ CentroEventos/CentroEventos.Aplicacion/Entidades/Persona.cs
@@ -2,10 +2,10 @@ namespace CentroEventos.Aplicacion;
 public class Persona
 {
     public int Id { get; set; }
-    public string Nombre { get; set; } = string.Empty;
-    public string DNI { get; set; } = string.Empty;
-    public string Apellido { get; set; } string.Empty;
-    public string Email { get; set; } string.Empty;
+    public string Nombre { get; set; }
+    public string DNI { get; set; } 
+    public string Apellido { get; set; } 
+    public string Email { get; set; }
 
     public override string ToString()
     {
