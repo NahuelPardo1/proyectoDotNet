f9abdc0b4e90becb9736c8853b8c2beeaea355a6
Nahuel <Nahuelpardo3@gmail.com>
2025-05-12 20:15:26 -0300

    añadiendo interfaz base

--- CentroEventos/CentroEventos.Aplicacion/Interfaces/IRepositorioPersona.cs
+++ CentroEventos/CentroEventos.Aplicacion/Interfaces/IRepositorioPersona.cs
@@ -1,13 +1,7 @@
 namespace CentroEventos.Aplicacion;
 
-public interface IRepositorioPersona
+public interface IRepositorioPersona : IRepositorioBase<Persona>
 {
-    void agregarPersona(Persona persona);
-    void modificarPersona(Persona persona);
-    void eliminarPersona(int id);
-    List<Persona> listarPersonas();
-    Persona obtenerPorID(int id);
     Persona obtenerPorDNI(string dni);
     Persona obtenerPorEmail(string email);
-
 }
