b55c3e6332d32e671c8e24c700b3d5c582d34ea8
Jonathan Hiriart <hiriartjonathan@gmail.com>
2025-05-14 17:32:46 -0300

    creando interface de servicioautorizacion y creando servicioAutorizacionProvisorio

---
+++ CentroEventos/CentroEventos.Aplicacion/Interfaces/IServicioAutorizacion.cs
@@ -0,0 +1,6 @@
+namespace CentroEventos.Aplicacion
+{
+    public interface IServicioAutorizacion // es provicional en esta entrega
+        bool PoseeElPermiso(int IdUsuario, Permiso permiso);
+    }
+}
