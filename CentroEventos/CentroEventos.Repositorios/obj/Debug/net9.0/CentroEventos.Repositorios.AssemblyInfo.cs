0fb9bac22a2a810ec58678e62e1d85a1c65c21af
Nahuel <Nahuelpardo3@gmail.com>
2025-05-13 23:24:49 -0300

    añado todos los casos de uso

--- CentroEventos/CentroEventos.Repositorios/obj/Debug/net9.0/CentroEventos.Repositorios.AssemblyInfo.cs
+++ CentroEventos/CentroEventos.Repositorios/obj/Debug/net9.0/CentroEventos.Repositorios.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CentroEventos.Repositorios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3047fa9e438933173375087566fa900c6db46f0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+467961d1538ac27af77abd37ee2adcc9b6cb8353")]
 [assembly: System.Reflection.AssemblyProductAttribute("CentroEventos.Repositorios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CentroEventos.Repositorios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
