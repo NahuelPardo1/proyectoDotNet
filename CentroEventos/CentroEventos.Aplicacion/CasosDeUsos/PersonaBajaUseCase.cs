8d10058b1bd5c85f97d11b15c5b4517585cef63d
Jonathan Hiriart <hiriartjonathan@gmail.com>
2025-05-14 22:44:22 -0300

    corrijiendo comentarios en PersonaBajaUseCase

--- CentroEventos/CentroEventos.Aplicacion/CasosDeUsos/PersonaBajaUseCase.cs
+++ CentroEventos/CentroEventos.Aplicacion/CasosDeUsos/PersonaBajaUseCase.cs
@@ -15,26 +15,30 @@ public class PersonaBajaUseCase
     }
     public void Ejecutar (int IDBaja, int IdUsuario)
     {
+        // 1. Verificar permiso
         if (_servicioAutorizacion.PoseeElPermiso(IdUsuario, Permiso.UsuarioBaja))
         {
             throw new FalloAutorizacionException("El usuario no posee el permiso para relizar esta acción");
         }
-        
-        Persona persona = _repositorioPersona.ObtenerPorId(IDBaja); // si la persona a eliminar no existe 
+        // 2. Verificar existencia de la persona
+        Persona persona = _repositorioPersona.ObtenerPorId(IDBaja); 
         if (persona == null)
         {
             throw new EntidadNotFoundException("La persona a eliminar no existe"); 
         }
-        List<Reserva> reservas = _repositorioReserva.ObtenerPorPersona(IDBaja); // si la persona tiene reservas 
-        if (reservas != null)
+        // 3. Verificar reservas asociadas
+        List<Reserva> reservas = _repositorioReserva.ObtenerPorPersona(IDBaja); 
+        if (reservas != null && reservas.Count >0)
         {
             throw new OperacionInvalidaException("El persona a eliminar tiene reservas");
         }
-        List<EventoDeportivo> eventos = _repositorioEventoDeportivo.ObtenerPorPersona(IDBaja); // si la persona tiene eventos deportivos
-        if (eventos != null)
+        // 4. Verificar si es responsable de algún evento deportivo
+        List<EventoDeportivo> eventos = _repositorioEventoDeportivo.ObtenerPorPersona(IDBaja);
+        if (eventos != null && eventos.Count> 0)
         {
             throw new OperacionInvalidaException("El persona a eliminar tiene eventos deportivos");
         }
+        // 5. Eliminar persona
         _repositorioPersona.Eliminar(IDBaja);
     }
 }
