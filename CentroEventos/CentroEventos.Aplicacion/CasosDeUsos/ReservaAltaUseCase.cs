786d26220d4ecb8e2a021af6c910cc1b5ae3adb9
Jonathan Hiriart <hiriartjonathan@gmail.com>
2025-05-15 14:10:03 -0300

    agregando Caso de Uso Reserva alta

---
+++ CentroEventos/CentroEventos.Aplicacion/CasosDeUsos/ReservaAltaUseCase.cs
@@ -0,0 +1,38 @@
+namespace CentroEventos.Aplicacion;
+public class ReservaAltaUseCase
+{
+    private readonly IRepositorioReserva _repositorioReserva;
+    private readonly IRepositorioPersona _repositorioPersona;
+    private readonly IRepositorioEventoDeportivo _repositorioEventoDeportivo;
+    private readonly IServicioAutorizacion _servicioAutorizacion;
+
+    public ReservaAltaUseCase(IRepositorioReserva repositorioReserva, IRepositorioPersona repositorioPersona,IServicioAutorizacion servicioAutorizacion , IRepositorioEventoDeportivo repositorioEventoDeportivo)
+    {
+        _repositorioReserva = repositorioReserva;
+        _repositorioPersona = repositorioPersona;
+        _repositorioEventoDeportivo = repositorioEventoDeportivo;
+        _servicioAutorizacion = servicioAutorizacion;
+    }
+    public void Ejecutar(Reserva datos, int IdUsuario)
+    {
+        // 1. Verificar permiso
+        if (! _servicioAutorizacion.PoseeElPermiso(IdUsuario,Persmiso.ReservaAlta))
+        { 
+            throw new FalloAutorizacionException("El usuario no posee el permiso para relizar esta acción");
+        }
+        // 2. Validar Reserva 
+        // 2.1 Verificar existencia de la persona
+        // 2.2 Verificar existencia del evento deportivo
+        // 2.3 Verificar que no exista una reserva para la misma persona y evento deportivo
+        // 2.4 Verificar que haya cupo disponible para el evento deportivo
+        ValidadorReserva validarReserva = new ValidadorReserva(_repositorioReserva,out string msj);
+        if (!validarReserva.Validar(datos, out msj))
+        {
+            throw new ValidacionException(msj);
+        }
+        // 3. Agregar reserva
+        _repositorioReserva.Agregar(datos);
+
+        // preguntar al profe si es correcto 
+    }
+}
